49b7a352fbbf440ab723c6aed279e99df5c70353
WitchBoy666 <krepyshevnikita8@gmail.com>
2024-02-09 19:20:58 +0300

    com4

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,21 @@ Route::get('/{year}/{month}/{day}', function($year, $month, $day) {
     }
 });
 
+Route::get('/user-city/{user}', function ($user) {
+    $users = [ 
+        'user1' => 'city1', 
+        'user2' => 'city2', 
+        'user3' => 'city3', 
+        'user4' => 'city4', 
+        'user5' => 'city5'
+    ];
+    if (array_key_exists($user, $users)) {
+        return $users[$user];
+    } else {
+        return "Пользователь с именем $user не найден";
+    }
+});
+
 Route::get('/user/{id?}', function ($id = 0) {
     return 'id ' . $id;
 });
